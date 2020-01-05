da24e57b81a3e4e7c24dbcdbe5be29ec8f51d550
Praveen <praveenics>
2020-01-05 23:32:29 +0530

    e2e script buyeth for new user

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -24,5 +24,3 @@
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
 
-import { addStreamCommands } from '@lensesio/cypress-websocket-testing';
-addStreamCommands();
